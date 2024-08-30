e9632fdcd9088a1a98ccba315d36dbeaf92bd7d4
leonardo.alvarenga <leonardo.alvarenga@anchieta.br>
2024-08-30 18:43:22 -0300

    Create Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import { Header } from "./components/Header";
 
 export function App() {
   return (
     <>
-     <h1>vamo ver se nao precisa puxar para ouutra pasta</h1>
-     <p>Teste</p>
+     <Header />
     </>
   )
 }
