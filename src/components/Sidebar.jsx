58d0884c71b332fe18fc08ca7fa4c610fb190c64
leonardo.alvarenga <leonardo.alvarenga@anchieta.br>
2024-09-03 18:47:18 -0300

    sidebar fix movitemntaçao

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { SidebarItem } from "./SidebarItem";
-import { FaBook, FaCircleNotch } from "react-icons/fa";
+import { RiDashboardFill, RiBook2Fill, RiSettings2Fill, RiCalendar2Fill, RiDashboard2Fill, RiFocusLine  } from "react-icons/ri";
 
 import logo from '../assets/logo.svg';
 
@@ -25,25 +25,32 @@ export function Sidebar() {
 
   return (
     <aside
-      className={`flex flex-col relative ${encolher || fixo ? 'w-96' : 'w-[84px]'} bg-zinc-950 transition-width duration-300 p-2`}
+      className={`flex flex-col relative ${encolher || fixo ? 'w-96' : 'w-[80px]'} bg-zinc-950 transition-width duration-300 p-2`}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      <div className={`flex items-center justify-center gap-5 pb-8 pt-8 border-b border-zinc-700`}>
-        <img src={logo} alt="logo" className={`w-18 `}/>
+      <div className={` border-zinc-700 pt-4 pb-8 border-b border-zinc-800 flex items-center gap-4`}>
+        <img src={logo} alt="" className='min-w-14'/>
+      
+        <h1 className={`text-2xl font-medium text-zinc-200 transition-all ${encolher || fixo ? 'opacity-100' : 'opacity-0'}`}>Organize</h1>
+        
+
+        {/* <img src={logo} alt="logo" className={`w-full max-w-16 ${encolher || fixo ? '': 'translate-x-0'}`}/>
         <h1
-          className={`text-2xl font-medium text-zinc-200 transition-all duration-300 whitespace-nowrap overflow-hidden ${encolher || fixo ? 'opacity-100' : 'opacity-0 delay-200'}`}
+          className={`text-2xl font-medium text-zinc-200 transition-all duration-300 whitespace-nowrap overflow-hidden ${encolher || fixo ? 'flex' : 'hidden'}`}
         >
           Organize
-        </h1>
+        </h1> */}
         <button onClick={botaoFixarMenu} className={encolher || fixo ? `absolute top-2 right-2 text-zinc-100 ` : 'hidden'}>
-          <FaCircleNotch size={24} className={fixo ? 'text-indigo-700' : 'text-zinc-100'}/>
+          <RiFocusLine  size={24} className={fixo ? 'text-indigo-700' : 'text-zinc-100'}/>
         </button>
       </div>
       <nav className="flex flex-col gap-4 mt-5">
-        <SidebarItem to='/' content='Início' icon={FaBook} hiddenOnHover={!encolher && !fixo} />
-        <SidebarItem to='/teste' content='AirplaneTilt' icon={FaBook} hiddenOnHover={!encolher && !fixo} />
-        <SidebarItem to='/teste2' content='Palette' icon={FaBook} hiddenOnHover={!encolher && !fixo} />
+        <SidebarItem to='/' content='Início' icon={RiDashboardFill } hiddenOnHover={!encolher && !fixo} />
+        <SidebarItem to='/teste' content='Matérias' icon={RiBook2Fill  } hiddenOnHover={!encolher && !fixo} />
+        <SidebarItem to='/teste2' content='Calendário' icon={RiCalendar2Fill  } hiddenOnHover={!encolher && !fixo} />
+        <SidebarItem to='/teste3' content='Desempenho' icon={RiDashboard2Fill  } hiddenOnHover={!encolher && !fixo} />
+        <SidebarItem to='/teste3' content='Configuração' icon={RiSettings2Fill  } hiddenOnHover={!encolher && !fixo} />
       </nav>
     </aside>
   );
--- src/components/SidebarItem.jsx
+++ src/components/SidebarItem.jsx
@@ -7,17 +7,28 @@ export function SidebarItem({ to, content, icon: Icon, hiddenOnHover }) {
 
   return (
     <Link to={to}>
-      <div
+      <div className={`flex w-full py-3 gap-3 rounded-xl text-zinc-300 ${isActive ? 'bg-indigo-700' : 'hover:bg-indigo-600'}`}>
+        <div className='ml-4'>
+          <Icon size={32}/>
+        </div>
+      <span
+          className={`text-xl overflow-hidden whitespace-nowrap transition-all duration-500 ease-out ${hiddenOnHover ? 'opacity-0' : 'opacity-100'}`}
+        >
+          {content}
+        </span>
+      </div>
+
+      {/* <div
         className={`flex items-center  text-xl w-full ${isActive ? 'bg-indigo-700' : 'hover:bg-indigo-600'} 
-          transition-all duration-300 rounded-xl text-zinc-300 group ${hiddenOnHover ? 'p-4 gap-4' : 'p-2 gap-2'}`}
+          transition-all duration-300 rounded-xl text-zinc-300 group ${hiddenOnHover ? 'p-4 gap-4' : 'p-4 gap-4'}`}
       >
-        <Icon size={36} weight='regular' className={`flex-shrink-0 ${hiddenOnHover ? 'scale-125' : 'scale-100'}`} />
+        <Icon size={32} weight='regular' className={`flex-shrink-0 ${hiddenOnHover ? 'scale-100' : 'scale-110'}`} />
         <span
           className={`overflow-hidden whitespace-nowrap transition-all duration-300 ${hiddenOnHover ? 'opacity-0' : 'opacity-100'}`}
         >
           {content}
         </span>
-      </div>
+      </div> */}
     </Link>
   );
 }
